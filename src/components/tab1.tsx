be6ab76f23f4b1c231f756c36c7ffdd7c4add9f3
Acpamitpathak <151603819+Acpamitpathak@users.noreply.github.com>
2024-08-31 21:35:08 +0530

    Create tab1.tsx

---
+++ src/components/tab1.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Tabs, Tab, Box, Typography } from '@mui/material';
+
+const LeftPage: React.FC = () => {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: '100%', height: '100%' }}>
+      <Tabs value={value} onChange={handleChange} aria-label="tabs">
+        <Tab label="Tab 1" />
+        <Tab label="Tab 2" />
+      </Tabs>
+      <Box sx={{ padding: 2 }}>
+        {value === 0 && (
+          <Typography variant="body1">
+            {/* Content for Tab 1 */}
+            <p>Details for Tab 1</p>
+            {/* You can add more complex components here */}
+          </Typography>
+        )}
+        {value === 1 && (
+          <Typography variant="body1">
+            {/* Content for Tab 2 */}
+            <p>Details for Tab 2</p>
+            {/* You can add more complex components here */}
+          </Typography>
+        )}
+      </Box>
+    </Box>
+  );
+};
+
+export default LeftPage;
