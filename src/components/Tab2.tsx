b9347aac9a10c7eec789674da2f14d7e3e2f7c9b
Acpamitpathak <151603819+Acpamitpathak@users.noreply.github.com>
2024-08-31 21:35:48 +0530

    Create Tab2.tsx

---
+++ src/components/Tab2.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+const Tab2: React.FC = () => {
+  return (
+    <Typography variant="body1">
+      {/* Content for Tab 2 */}
+      <p>Details for Tab 2</p>
+      {/* You can add more complex components here */}
+    </Typography>
+  );
+};
+
+export default Tab2;
